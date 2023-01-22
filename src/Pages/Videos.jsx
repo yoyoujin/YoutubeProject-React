fc291f00e2370491df5eac7c2da377436c4f2654
youjin <yjinhann@gmail.com>
2023-01-22 20:43:05 +0900

    feat: API 통신코드 구현

--- src/Pages/Videos.jsx
+++ src/Pages/Videos.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import VideoCard from '../components/VideoCard';
-import { search } from '../api/youtube';
+import Youtube, { search } from '../api/youtube';
 import FakeYoutube from '../api/fakeyoutube';
 
 const Videos = () => {
